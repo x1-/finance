a0ee041e7f160cb842578036feb3523dd2762c95
x1- <viva008@gmail.com>
2017-03-07 10:02:51 +0900

    calculating upper rate

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,18 @@ extern crate time;
 #[macro_use]
 extern crate lazy_static;
 
-use std::str::FromStr;
 use chrono::prelude::*;
 use regex::Regex;
-use regex::Match;
 use time::Duration;
 
 mod api_client;
 
-//static URL: &'static str = "https://www.google.co.jp/";
-//static url_base: &'static str = "http://www.google.com/finance/getprices?p={term}&f=d,h,o,l,c,v&i={tick}&x={market}&q={code}";
-
 static INTERVAL: i64 = 86400;
 const TIME_FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
+const URL_BASE: &'static str = "http://www.google.com/finance/getprices";
 
 lazy_static! {
-    static ref splitter_reg: Regex = Regex::new( r"TIMEZONE_OFFSET=\d+\n" ).unwrap();
+    static ref SPLITTER_REG: Regex = Regex::new( r"TIMEZONE_OFFSET=\d+\n" ).unwrap();
 }
 
 #[derive(RustcDecodable)]
@@ -47,53 +43,6 @@ struct CsvRow {
 
 fn main() {
 
-    // let url = match env::args().nth(1) {
-    //     Some(url) => url,
-    //     None => {
-    //         println!("Usage: client <url>");
-    //         return;
-    //     }
-    // };
-
-    // let client = match env::var("HTTP_PROXY") {
-    //     Ok(mut proxy) => {
-    //         let mut port = 80;
-    //         if let Some(colon) = proxy.rfind(':') {
-    //             port = proxy[colon + 1..].parse().unwrap_or_else(|e| {
-    //                 panic!("HTTP_PROXY is malformed: {:?}, port parse error: {}", proxy, e);
-    //             });
-    //             proxy.truncate(colon);
-    //         }
-    //         Client::with_http_proxy(proxy, port)
-    //     },
-    //     _ => Client::new()
-    // };
-
-    // let mut res = client.get(URL)
-    //     .header(Connection::close())
-    //     .send().unwrap();
-
-    // println!("Response: {}", res.status);
-    // println!("Headers:\n{}", res.headers);
-    // io::copy(&mut res, &mut io::stdout()).unwrap();
-
-    let tmp2 = DateTime::parse_from_str("2017-01-01 09:00:00 +09:00", "%Y-%m-%d %H:%M:%S %z");
-    println!( "{}", tmp2.ok().unwrap().format("%s").to_string() );
-
-    // let tmp = DateTime::parse_from_str("1486015200", "%s");
-    let tmp = time::strptime("1483228800", "%s");;
-    println!( "{:?}", tmp.ok() );
-
-    let tmp3 = Local.timestamp( "1486015200".parse::<i64>().unwrap(), 0 );
-    println!( "{:?}", tmp3 );
-    println!( "{}", tmp3.format("%Y-%m-%d %H:%M:%S").to_string() );
-
-    let tmp4 = tmp3 + Duration::seconds( 86400 );
-    println!( "{:?}", tmp4 );
-    println!( "{}", tmp4.format("%Y-%m-%d %H:%M:%S").to_string() );
-
-
-
     let client = api_client::Ssl::new();
 
     let mut file = csv::Reader::from_file("./data/stocks.csv").unwrap();
@@ -102,62 +51,21 @@ fn main() {
         let r: Record = r.unwrap();
 
         let url = format!(
-            "http://www.google.com/finance/getprices?p={term}&f=d,h,o,l,c,v&i={tick}&x={market}&q={code}",
+            "{uri}?f=d,h,o,l,c,v&p={term}&i={tick}&x={market}&q={code}",
+            uri = URL_BASE,
             term = "7d", tick = INTERVAL, market = "TYO", code = r.code );
+
         let res = &client.sync_get( &url );
 
-        let data = splitter_reg.split( res ).last().and_then( transform_csv );
-        if data.is_none() {
-            break;
-        };
-
-        let mut new_data = Vec::new();
-        let mut base_time: Option<DateTime<Local>> = None;
-
-        for row in data.unwrap() {
-
-            let ref date = row.date;
-            if let Ok(new_date) = calc_time( date, INTERVAL, &mut base_time ) {
-                println!("date: {}", new_date);
-                let new_row = CsvRow {
-                    date  : new_date,
-                    close : row.close,
-                    high  : row.high,
-                    low   : row.low,
-                    open  : row.open,
-                    volume: row.volume
-                };
-                new_data.push( new_row );
-            }
-        };
-
-        let rate = close_rate( &new_data );
+        let rate = data_to_struct( res ).and_then(|dt| close_rate( &dt ));
         if let Ok(x) = rate {
-            println!("rate: {}", x);
+            println!("rate: {:?}", x);
         }
-
-// EXCHANGE%3DTYO
-// MARKET_OPEN_MINUTE=540
-// MARKET_CLOSE_MINUTE=900
-// INTERVAL=86400
-// COLUMNS=DATE,CLOSE,HIGH,LOW,OPEN,VOLUME
-// DATA_SESSIONS=[MORNING,540,690],[AFTERNOON,750,900]
-// DATA=
-// TIMEZONE_OFFSET=540
-// a1484632800,1104,1124,1103,1121,34500
-
-        // let lines = res.split("\n");
-        // for row in lines {
-        //     println!( "{}", row );
-        //     break;
-        // }
-
-        // println!( "{}", res );
     }
 }
 
-fn close(maybeRow: Option<&CsvRow>) -> Result<f32, String> {
-    maybeRow.map( |r|r.close ).ok_or( "cannot get row or row.close".to_string() )
+fn close(maybe_row: Option<&CsvRow>) -> Result<f32, String> {
+    maybe_row.map( |r|r.close ).ok_or( "cannot get row or row.close".to_string() )
 }
 
 fn close_rate(vec: &Vec<CsvRow>) -> Result<f32, String> {
@@ -177,11 +85,11 @@ fn transform_csv(data: &str) -> Result<Vec<CsvRow>, String> {
         .map_err( |e|e.to_string() )
 }
 
-fn data_to_struct(res: &str) -> Result<Vec<CsvRow>, String>{
+fn data_to_struct(res: &str) -> Result<Vec<CsvRow>, String> {
     let mut new_data: Vec<CsvRow> = Vec::new();
     let mut base_time: Option<DateTime<Local>> = None;
 
-    let data = splitter_reg.split( res ).last()
+    let data = SPLITTER_REG.split( res ).last()
         .ok_or( "cannot get data section".to_string() )
         .and_then( transform_csv )?;
 
@@ -201,7 +109,7 @@ fn data_to_struct(res: &str) -> Result<Vec<CsvRow>, String>{
             new_data.push( new_row );
         }
     };
-    return new_data
+    return Ok(new_data);
 }
 
 fn local_time(s: &str) -> Result<DateTime<Local>, String> {
