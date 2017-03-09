c8a52e996ac85012a0a25b5bdd9630b9db165924
x1- <viva008@gmail.com>
2017-03-09 13:22:37 +0900

    add docopt.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate chrono;
 extern crate csv;
+extern crate docopt;
 extern crate env_logger;
 extern crate hyper;
 extern crate hyper_openssl;
@@ -12,30 +13,50 @@ extern crate time;
 extern crate lazy_static;
 
 use chrono::prelude::*;
+use docopt::Docopt;
 use regex::Regex;
 use slack_hook::{Slack, Payload, PayloadBuilder};
 use time::Duration;
 
 mod api_client;
 
-static INTERVAL: i64 = 86400;
-const TIME_FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
-const URL_BASE: &'static str = "http://www.google.com/finance/getprices";
 
-const SLACK_WEBHOOK: &'static str = "";
+const USAGE: &'static str = r"
+to notice kabu rate of up or down at slack-channel.
+Usage:
+  finance --webhook=<url> [--term=<term>] [--tick=<tick>]
+  finance --version
+Options:
+  -h --help       Show this message.
+  --version       Show version.
+  --webhook=<url> webhook url of slack integration.
+  --term=<term>   the term of measuring price [default 7d].
+  --tick=<tick>   candle tick interval by seconds [default 86400].
+";
+
+
+const TIME_FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
+const URL_BASE: &'static str = "http://www.google.com/finance/getprices?f=d,h,o,l,c,v";
 
 lazy_static! {
     static ref SPLITTER_REG: Regex = Regex::new( r"TIMEZONE_OFFSET=\d+\n" ).unwrap();
 }
 
-#[derive(RustcDecodable)]
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    flag_webhook: String,
+    flag_term   : String,
+    flag_tick   : i64
+}
+
+#[derive(Debug, RustcDecodable)]
 struct Record {
     code: String,
     name: String,
     market: String
 }
 
-#[derive(RustcDecodable)]
+#[derive(Debug, RustcDecodable)]
 struct CsvRow {
     date  : String,
     close : f32,
@@ -45,10 +66,22 @@ struct CsvRow {
     volume: u64
 }
 
+#[derive(Debug)]
+struct ComparedPrice {
+    current  : f32,
+    previous : f32,
+    ratio    : f32
+}
+
 fn main() {
 
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
+
     let client = api_client::Ssl::new();
-    let slack = Slack::new(SLACK_WEBHOOK).unwrap();
+    let slack = Slack::new( args.flag_webhook.as_str() ).unwrap();
 
     let mut file = csv::Reader::from_file("./data/stocks.csv").unwrap();
 
@@ -56,18 +89,18 @@ fn main() {
         let r: Record = r.unwrap();
 
         let url = format!(
-            "{uri}?f=d,h,o,l,c,v&p={term}&i={tick}&x={market}&q={code}",
+            "{uri}&p={term}&i={tick}&x={market}&q={code}",
             uri = URL_BASE,
-            term = "7d", tick = INTERVAL, market = "TYO", code = r.code );
+            term = args.flag_term, tick = args.flag_tick, market = "TYO", code = r.code );
 
         let res = &client.sync_get( &url );
 
-        let rdata = data_to_struct( res );
-        let ratex = rdata.and_then( |data| close_rate( &data ) );
+        let data = data_to_struct( res, args.flag_tick );
+        let rprice = data.and_then( |d| close_rate( &d ) );
 
-        match ratex {
-            Ok((rate, x)) if rate >= 0.1 || rate < -0.1 => {
-                let payload = slack_payload( r.code, r.name, x, rate );
+        match rprice {
+            Ok( ref p ) if p.ratio >= 0.1 || p.ratio < -0.1 => {
+                let payload = slack_payload( r.code, r.name, p.current, p.previous, p.ratio );
                 slack.send( &payload );
                 println!("payload: {:?}", payload);
             },
@@ -80,12 +113,17 @@ fn close(maybe_row: Option<&CsvRow>) -> Result<f32, String> {
     maybe_row.map( |r|r.close ).ok_or( "cannot get row or row.close".to_string() )
 }
 
-fn close_rate(vec: &Vec<CsvRow>) -> Result<(f32, f32), String> {
+fn close_rate(vec: &Vec<CsvRow>) -> Result<ComparedPrice, String> {
     let ( before, now ) = ( try!(close(vec.first())), try!(close(vec.last())) );
     if before == 0.0 {
         Err( "close value of first row is zero (0.0)".to_string() )
     } else {
-        Ok( ( (now - before) / before, now ) )
+        let price = ComparedPrice {
+            current: now,
+            previous: before,
+            ratio: (now - before) / before
+        };
+        Ok( price )
     }
 }
 
@@ -96,7 +134,7 @@ fn transform_csv(data: &str) -> Result<Vec<CsvRow>, String> {
         .map_err( |e|e.to_string() )
 }
 
-fn data_to_struct(res: &str) -> Result<Vec<CsvRow>, String> {
+fn data_to_struct(res: &str, interval: i64) -> Result<Vec<CsvRow>, String> {
     let mut new_data: Vec<CsvRow> = Vec::new();
     let mut base_time: Option<DateTime<Local>> = None;
 
@@ -107,7 +145,7 @@ fn data_to_struct(res: &str) -> Result<Vec<CsvRow>, String> {
     for row in data {
 
         let ref date = row.date;
-        if let Ok(new_date) = calc_time( date, INTERVAL, &mut base_time ) {
+        if let Ok(new_date) = calc_time( date, interval, &mut base_time ) {
             let new_row = CsvRow {
                 date  : new_date,
                 close : row.close,
@@ -151,10 +189,10 @@ fn calc_time(raw_value: &String, interval: i64, base_time: &mut Option<DateTime<
     Ok( time.format(TIME_FORMAT).to_string() )
 }
 
-fn slack_payload(code: String, name: String, price: f32, rate: f32) -> Payload {
+fn slack_payload(code: String, name: String, current: f32, previous: f32, rate: f32) -> Payload {
     let emoji = if rate > 0.0 { ":chart_with_upwards_trend:" } else { ":chart_with_downwards_trend:" };
 
-    let message = format!("Now, {code}:{name} price {price} ( change rate is {rate} from last week ).",
+    let message = format!("{code}:{name} 現在値:￥{current} ( 先週からの変化率 {rate}, 先週の値:￥{previous} ).",
                           code = code,
                           name = name,
                           price = price,
